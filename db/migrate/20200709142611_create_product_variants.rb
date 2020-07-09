a62458bc25db50828e6fcf17ca0174d67d4f7eee
Mathieu Duprez <math.dups@gmail.com>
2020-07-09 16:57:46 +0200

    schema

--- db/migrate/20200709142611_create_product_variants.rb
+++ db/migrate/20200709142611_create_product_variants.rb
@@ -1,9 +1,7 @@
-class CreateProducts < ActiveRecord::Migration[6.0]
+class CreateProductVariants < ActiveRecord::Migration[6.0]
   def change
-    create_table :products do |t|
+    create_table :product_variants do |t|
       t.string :title, null: false
-      t.text :description
-      t.string :image_url
       t.decimal :price, null: false, precision: 6, scale: 2
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_07_232500) do
+ActiveRecord::Schema.define(version: 2020_07_09_143453) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity", default: 0, null: false
+    t.decimal "price", precision: 6, scale: 2, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "order_id", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name", null: false
+    t.decimal "sub_total", precision: 6, scale: 2, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
+
+  create_table "product_variants", force: :cascade do |t|
+    t.string "title", null: false
+    t.decimal "price", precision: 6, scale: 2, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "product_id", null: false
+    t.index ["product_id"], name: "index_product_variants_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -33,4 +74,8 @@ ActiveRecord::Schema.define(version: 2020_07_07_232500) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
+  add_foreign_key "product_variants", "products"
 end
