87f2813c90c5088a929d7246a1a7d8e9684def76
Mathieu Duprez <math.dups@gmail.com>
2020-07-09 21:12:40 +0200

    seed and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :products
+
   devise_for :users
   root to: 'products#index'
+
+
+  resources :products
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
